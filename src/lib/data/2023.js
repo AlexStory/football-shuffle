75abc3fb70ec2ca2b2fd9ed8384063f382c79af8
Alex Story <astory11@yahoo.com>
2024-08-30 18:50:01 -0500

    initial commit

---
+++ src/lib/data/2023.js
@@ -0,0 +1,133 @@
+const data = [
+    // 2023
+    {
+        "year": 2023,
+        "week": 1,
+        "teams": [
+            "Jacksonville Jaguars",
+            "Indianapolis Colts"
+        ]
+    },
+    {
+        "year": 2023,
+        "week": 1,
+        "teams": [
+            "Detroit Lions",
+            "Kansas City Chiefs"
+        ]
+    },
+    {
+        "year": 2023,
+        "week": 1,
+        "teams": [
+            "Carolina Panthers",
+            "Atlanta Falcons"
+        ]
+    },
+    {
+        "year": 2023,
+        "week": 1,
+        "teams": [
+            "Houston Texans",
+            "Baltimore Ravens"
+        ]
+    },
+    {
+        "year": 2023,
+        "week": 1,
+        "teams": [
+            "Cincinnati Bengals",
+            "Cleveland Browns"
+        ]
+    },
+    {
+        "year": 2023,
+        "week": 1,
+        "teams": [
+            "Tampa Bay Buccaneers",
+            "Minnesota Vikings"
+        ]
+    },
+    {
+        "year": 2023,
+        "week": 1,
+        "teams": [
+            "Tennessee Titans",
+            "New Orleans Saints"
+        ]
+    },
+    {
+        "year": 2023,
+        "week": 1,
+        "teams": [
+            "San Francisco 49ers",
+            "Pittsburgh Steelers"
+        ]
+    },
+    {
+        "year": 2023,
+        "week": 1,
+        "teams": [
+            "Arizona Cardinals",
+            "Washington Commanders"
+        ]
+    },
+    {
+        "year": 2023,
+        "week": 1,
+        "teams": [
+            "Green Bay Packers",
+            "Chicago Bears"
+        ]
+    },
+    {
+        "year": 2023,
+        "week": 1,
+        "teams": [
+            "Denver Broncos",
+            "Las Vegas Raiders"
+        ]
+    },
+    {
+        "year": 2023,
+        "week": 1,
+        "teams": [
+            "Miami Dolphins",
+            "Los Angeles Chargers"
+        ]
+    },
+    {
+        "year": 2023,
+        "week": 1,
+        "teams": [
+            "Philadelphia Eagles",
+            "New England Patriots"
+        ]
+    },
+    {
+        "year": 2023,
+        "week": 1,
+        "teams": [
+            "Los Angeles Rams",
+            "Seattle Seahawks"
+        ]
+    },
+    {
+        "year": 2023,
+        "week": 1,
+        "teams": [
+            "New York Giants",
+            "Dallas Cowboys"
+        ]
+    },
+    {
+        "year": 2023,
+        "week": 1,
+        "teams": [
+            "Buffalo Bills",
+            "New York Jets"
+        ]
+    }
+]
+
+export default data;
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,34 +1,130 @@
-<script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
+<script lang="ts">
+	type Game = {
+		teams: string[];
+		year: number;
+		week: number;
+	};
+	import data2023 from '../lib/data/2023';
+
+	const data = data2023;
+
+	let teams: string[] = $state([
+		'Arizona Cardinals',
+		'Atlanta Falcons',
+		'Baltimore Ravens',
+		'Buffalo Bills',
+		'Carolina Panthers',
+		'Chicago Bears',
+		'Cincinnati Bengals',
+		'Cleveland Browns',
+		'Dallas Cowboys',
+		'Denver Broncos',
+		'Detroit Lions',
+		'Green Bay Packers',
+		'Houston Texans',
+		'Indianapolis Colts',
+		'Jacksonville Jaguars',
+		'Kansas City Chiefs',
+		'Las Vegas Raiders',
+		'Los Angeles Chargers',
+		'Los Angeles Rams',
+		'Miami Dolphins',
+		'Minnesota Vikings',
+		'New England Patriots',
+		'New Orleans Saints',
+		'New York Giants',
+		'New York Jets',
+		'Philadelphia Eagles',
+		'Pittsburgh Steelers',
+		'San Francisco 49ers',
+		'Seattle Seahawks',
+		'Tampa Bay Buccaneers',
+		'Tennessee Titans',
+		'Washington Commanders'
+	]);
+	let selectedTeam = $state('');
+	let selectedYear = $state('');
+
+	let filtered = $derived(
+		data.filter((game) => {
+			if (selectedTeam && selectedYear) {
+				return game.teams.includes(selectedTeam) && game.year * 1 === +selectedYear;
+			} else if (selectedTeam) {
+				return game.teams.includes(selectedTeam);
+			} else {
+				return game;
+			}
+		})
+	);
+
+	let selectedGame: Game = $state({ teams: [], year: 0, week: 0 });
+
+	function shuffle() {
+		const randomIndex = Math.floor(Math.random() * filtered.length);
+		const randomGame = filtered[randomIndex];
+		selectedGame = randomGame;
+	}
 </script>
 
 <svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+	<title>Football Shuffle</title>
+	<meta name="description" content="Football Shuffle" />
 </svelte:head>
 
 <section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
+	<p>Optionally pick a team, and year and get a random game picked to watch!</p>
 
-		to your new<br />SvelteKit app
-	</h1>
+	<div>
+		<label for="team">Team:</label>
+		<select bind:value={selectedTeam} name="team">
+			{#each teams as team}
+				<option value={team}>{team}</option>
+			{/each}
+		</select>
+		<button onclick={() => (selectedTeam = '')}>Clear</button>
+	</div>
 
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
+	<div>
+		<label for="year">Year:</label>
+		<select bind:value={selectedYear} name="year">
+			{#each Array.from({ length: 2024 - 2008 }, (_, i) => new Date().getFullYear() - i) as year}
+				<option value={year}>{year}</option>
+			{/each}
+		</select>
+		<button onclick={() => (selectedYear = '')}>Clear</button>
+	</div>
 
-	<Counter />
+	<button onclick={shuffle} class="shuffle"> SHUFFLE! </button>
+
+	{#if selectedGame.year > 0}
+		<p>{selectedGame.teams.join(' vs ')}</p>
+		<p>{selectedGame.year}</p>
+		<p>week: {selectedGame.week}</p>
+	{/if}
 </section>
 
 <style>
+	button {
+		cursor: pointer;
+		background-color: #3498db;
+		font-weight: bold;
+		color: white;
+		border: none;
+		border-radius: 8px;
+		padding: 5px 8px;
+	}
+
+	p {
+		border-bottom: 1px solid #ccc;
+	}
+
+	select {
+		background-color: white;
+		border: 1px solid #ccc;
+		padding: 5px;
+		border-radius: 2px;
+	}
+
 	section {
 		display: flex;
 		flex-direction: column;
@@ -37,23 +133,10 @@
 		flex: 0.6;
 	}
 
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
+	.shuffle {
+		min-width: 200px;
+		margin-top: 10px;
+		padding-top: 15px;
+		padding-bottom: 15px;
 	}
 </style>
